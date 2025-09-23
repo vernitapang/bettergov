a3de8603e959b177f35b76f1924ed65312304e9a
Ivan Justyn Austria <95930075+ivanjustyn-dev@users.noreply.github.com>
2025-09-24 00:51:36 +0800

    Feat(logo): update logo for header and footer (#228)
    * feat(logo): add logo assets and update Header and Footer Logo
    
    * feat(logo): fix misupdated imports
    
    * feat(logo): remove commented out code

--- src/components/layout/Footer.tsx
+++ src/components/layout/Footer.tsx
@@ -1,5 +1,4 @@
 import React from 'react';
-import { CheckCircle2 } from 'lucide-react';
 import { footerNavigation } from '../../data/navigation';
 import { Link } from 'react-router-dom';
 import { useTranslation } from 'react-i18next';
@@ -34,12 +33,11 @@ const Footer: React.FC = () => {
         <div className='grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-8'>
           <div>
             <div className='flex items-center mb-4'>
-              <CheckCircle2 className='h-12 w-12 mr-3' />
-              {/* <img
-                src="/ph-logo.webp"
-                alt="Philippines Coat of Arms"
-                className="h-12 w-12 mr-3"
-              /> */}
+              <img
+                src='/logos/svg/BetterGov_Icon-White.svg'
+                alt='BetterGov Logo'
+                className='h-12 w-12 mr-3'
+              />
 
               <div>
                 <div className='font-bold'>Better Philippines</div>
