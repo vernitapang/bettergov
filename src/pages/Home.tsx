729d604999e0265a97cdef9026548d25bcae1581
aocosmic <51456343+cybrejon@users.noreply.github.com>
2025-09-24 12:33:56 +0800

    refactor: remove (redundant) data section (#107)
    * refactor: remove (redundant) data section and move the heading into info widgets section
    
    * refactor: remove unused DataSection
    
    ---------
    
    Co-authored-by: codegino <carloginocatapang@gmail.com>

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -6,7 +6,6 @@ import InfoWidgets from '../components/home/InfoWidgets';
 import PromotionBanner from '../components/home/PromotionBanner';
 import JoinUsBanner from '../components/home/JoinUsBanner';
 import GovernmentSection from '../components/home/GovernmentSection';
-import DataSection from '../components/home/DataSection';
 
 const Home: React.FC = () => {
   return (
@@ -16,7 +15,6 @@ const Home: React.FC = () => {
       <ServicesSection />
       {/* <NewsSection /> */}
       <InfoWidgets />
-      <DataSection />
       <JoinUsBanner />
       <PromotionBanner />
       <GovernmentSection />
